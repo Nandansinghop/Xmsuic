3237a688106c3ea75e57fd52324401efa47b6039
Nandansinghop <119552033+Nandansinghop@users.noreply.github.com>
2023-11-04 19:04:10 +0530

    Update play.py

--- DAXXMUSIC/utils/decorators/play.py
+++ DAXXMUSIC/utils/decorators/play.py
@@ -37,7 +37,7 @@ def PlayWrapper(command):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="DAXXmousAdmin",
+                            callback_data="LxF_MR_NEON",
                         ),
                     ]
                 ]
